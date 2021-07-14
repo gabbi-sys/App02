e938d791c66132997edd88f01b152d4eb3cb3900
gabbi-sys <58314040+gabbi-sys@users.noreply.github.com>
2021-07-14 20:34:42 -0300

    14.07

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 // 1) Importa dependências
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -11,35 +12,37 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
   // 3) Atributos do script
-  private itemsCollection: AngularFirestoreCollection;
   public items: Observable<any>;
+  public pipe = new DatePipe('en_US'); // Formatar as datas
 
   constructor(
     // 2) Injeta dependências
     private afs: AngularFirestore,
     public auth: AngularFireAuth
   ) {
+    // Obtém data de agora
+    var now = this.pipe.transform(Date.now(), 'yyyy-MM-dd HH:mm:ss').trim();
 
     // 4) Acessa e obtém dados da coleção
-    this.itemsCollection = afs.collection(
-      'articles', // Coleção a ser consultada
-      ref => ref // Aplica filtros
-        .where('status', '==', 'ativo') // Somente com 'status'='ativo'
-        .orderBy('date', 'desc') // Ordena por 'date' na ordem decrescente
+    this.items = this.afs
+      .collection(
+        'articles', // Coleção a ser consultada
+        (ref) =>
+          ref // Aplica filtros
+            .where('status', '==', 'ativo') // Somente com 'status'='ativo'
+            .where('date', '<=', now) // Somente com a data no passado
+            .orderBy('date', 'desc') // Ordena por 'date' na ordem decrescente
 
-      /*
+        /*
         ATENÇÃO!
           Será necessário gerar um índice no Firestore para que esta query funcione.
           O link para gerar o índice aparece no console.
           Logue-se no Firebase.com e clique no link do console.
       */
-     );
-    this.items = this.itemsCollection.valueChanges({ idField: 'id' });
-
+      )
+      .valueChanges({ idField: 'id' });
   }
 
-  ngOnInit() { }
-
+  ngOnInit() {}
 }
